e05139616368ab12720e220fee915136ba315d81
B3dRandoms <bdrandoms0@gmail.com>
2023-12-26 14:42:42 +0200

    Initial commit

---
+++ Lab1-3Dudka/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите n");
+long n = Convert.ToInt64(Console.ReadLine());
+long hours = n / 3600;
+long minutes = (n % 3600) / 60;
+long seconds = (n % 3600) % 60;
+Console.WriteLine($"часы {hours}");
+Console.WriteLine($"минуты {minutes}");
+Console.WriteLine($"секунды {seconds}");
